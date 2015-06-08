5359b04165a579844a9eb1fb8fd8b158816071c3
sebita123 <seebastianaleejandro@live.com>
2015-06-08 07:45:05 -0400

    haciendo cambios

--- mijavascript.js
+++ mijavascript.js
@@ -1,5 +1,7 @@
 function inicial(){
-
+$("#hide1").css("visibility","visible").hide();
+$("#hide2").css("visibility","visible").hide();
+$("#hide3").css("visibility","visible").hide();
 $("body").hide().fadeIn(2000);
 
 $(".margen1").hide(0).toggle("bounce",2000).shadow("raised");
@@ -13,42 +15,38 @@ $(".explode").mouseover(function(){
 	$(".cont1").shadow("raised");
 	$(".cont2").shadow("raised");
 	$(".cont3").shadow("raised");
-	$("#primero").hide();
+	
+	
+
 $(".desplazar").hide().toggle("slide",2000);
-$("#primero").hide().toggle("bounce",2000);
-$(".medium").hide().toggle("bounce",2000);
+$("#primero").hide().toggle("bounce",3000);
+$("#segundo").hide().toggle("bounce",3000);
+$("#tercero").hide().toggle("bounce",3000);
+$(".medium").hide().toggle("bounce",3000);
 
 			$(".cont1").mouseover(function(){
 		$(this).hide(0).toggle("highlight",1000)
 		
 });
 $(".cont2").mouseover(function(){
-		$(this).hide(0).toggle("highlight",1000)});
+		$(this).hide(0).toggle("highlight",1)});
 					$(".cont3").mouseover(function(){
 		$(this).hide(0).toggle("highlight",1000)});
 		
 		$("li").shadow("raised");
-	
-	
 
-
-$(".aparecer").hide();
-$("#descripcion1").mouseover(function(){
-	
-	$(".aparecer").toggle();
-	
-	});
-	$("#descripcion1").mouseleave(function(){
-	
-	$(".aparecer").hide();
-	
-	});
 	
 	$("li").mouseover(function(){
 		$(this).hide(0).toggle("highlight",1000);
 		
 		});
-
-
+		
+		
+		
+		
+		$("#queescch").click(function(){
+			
+			$("#hide1").slideToggle(1000).shadow("raised").html("el lenguaje c#").css("font-family","Verdana, Geneva, sans-serif").css("color","#333").append("<div><h1> hello world</h1></div>");
+			});
+		
 }
-inicial();
